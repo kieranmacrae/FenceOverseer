4a3f54f5e5e9cfdf1ed3cbd541e6b94215d9c084
Kieran MacRae <macrae.kieran@gmail.com>
2016-08-12 12:20:05 +1000

    Text field and button for New unit registration

---
+++ app/src/main/java/com/example/kieran/fenceoverseer/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.kieran.fenceoverseer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+/**
+ * Created by Kieran on 23/07/2016.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
